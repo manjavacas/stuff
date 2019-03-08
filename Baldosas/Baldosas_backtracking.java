c6dac56a93d4e0a9f7999ad8f6e99f679f6032e8
Antonio <40355800+manjavacas@users.noreply.github.com>
2019-03-08 20:48:40 +0100

    Update Baldosas_backtracking.java

--- Baldosas/Baldosas_backtracking.java
+++ Baldosas/Baldosas_backtracking.java
@@ -1,4 +1,3 @@
-package p4;
 
 import java.util.Arrays;
 import Utilidades.*;
@@ -57,7 +56,7 @@ public class Baldosas_backtracking {
 		int n_tipos;
 		int[] baldosas;
 
-		n_tipos = leer.entero("Introducir número de tipos de baldosa: ");
+		n_tipos = leer.entero("Introducir nÃºmero de tipos de baldosa: ");
 		baldosas = new int[n_tipos];
 
 		/* Lectura de dimensiones */

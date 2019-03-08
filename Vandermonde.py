ac6cf123b0118292cdbdb47c3d0c9f3742bada3c
Antonio <40355800+manjavacas@users.noreply.github.com>
2019-03-08 20:55:01 +0100

    Add files via upload

---
+++ Vandermonde.py
@@ -0,0 +1,18 @@
+
+n = 10
+
+# Crear matriz
+Matrix = [[0 for x in range(n)] for y in range(n)] 
+
+# Rellenar matriz
+for i in range(0, n):
+    for j in range(0, n):
+        Matrix[i][j] = (i+1)**j
+
+# Mostrar matriz
+for i in range(n):
+    for j in range(n):
+        print(Matrix[i][j], end='\t')
+    print()
+
+
